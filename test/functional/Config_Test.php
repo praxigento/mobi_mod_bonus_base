ee624e0ae564c79a4851f13863f0112181b94db4
Alex Gusev <alex@flancer64.com>
2015-10-15 19:25:58 +0300

    MOBI-101 - Create repos for critical path

---
+++ test/functional/Config_Test.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Empty class to get stub for tests
+ * User: Alex Gusev <alex@flancer64.com>
+ */
+namespace Praxigento\Bonus\Base;
+include_once(__DIR__ . '/phpunit_bootstrap.php');
+
+class Config_UnitTest extends \PHPUnit_Framework_TestCase {
+
+    public function test_lib() {
+        $ctx = \Praxigento\Bonus\Base\Lib\Context::get();
+        $this->assertTrue($ctx instanceof \Praxigento\Core\Lib\Context);
+    }
+
+}
