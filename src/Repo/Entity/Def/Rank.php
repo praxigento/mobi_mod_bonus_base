630dd8e99e8871aacd26e6296b0c601abd0ddbdb
Alex Gusev <alex@flancer64.com>
2016-06-30 20:30:39 +0300

    MOBI-327 - Refactoring for Repo interfaces

--- src/Repo/Entity/Def/Rank.php
+++ src/Repo/Entity/Def/Rank.php
@@ -2,20 +2,20 @@
 /**
  * User: Alex Gusev <alex@flancer64.com>
  */
-
 namespace Praxigento\BonusBase\Repo\Entity\Def;
 
-use Praxigento\Bonus\Base\Lib\Entity\Rank as Entity;
-use Praxigento\BonusBase\Repo\Entity\IRank;
-use Praxigento\Core\Repo\Def\Type as BaseType;
-
-class Rank extends BaseType implements IRank
+class Rank
+    extends \Praxigento\Core\Repo\Entity\Def\Type
+    implements \Praxigento\BonusBase\Repo\Entity\IRank
 {
-
     public function __construct(
         \Magento\Framework\App\ResourceConnection $resource,
         \Praxigento\Core\Repo\IGeneric $repoGeneric
     ) {
-        parent::__construct($resource, $repoGeneric, Entity::class);
+        parent::__construct(
+            $resource,
+            $repoGeneric,
+            \Praxigento\Bonus\Base\Lib\Entity\Rank::class
+        );
     }
 }
--- src/Repo/Entity/Type/Def/Calc.php
+++ src/Repo/Entity/Type/Def/Calc.php
@@ -2,20 +2,20 @@
 /**
  * User: Alex Gusev <alex@flancer64.com>
  */
-
 namespace Praxigento\BonusBase\Repo\Entity\Type\Def;
 
-use Praxigento\Bonus\Base\Lib\Entity\Type\Calc as Entity;
-use Praxigento\BonusBase\Repo\Entity\Type\ICalc;
-use Praxigento\Core\Repo\Def\Type as BaseType;
-
-class Calc extends BaseType implements ICalc
+class Calc
+    extends \Praxigento\Core\Repo\Entity\Def\Type
+    implements \Praxigento\BonusBase\Repo\Entity\Type\ICalc
 {
-
     public function __construct(
         \Magento\Framework\App\ResourceConnection $resource,
         \Praxigento\Core\Repo\IGeneric $repoGeneric
     ) {
-        parent::__construct($resource, $repoGeneric, Entity::class);
+        parent::__construct(
+            $resource,
+            $repoGeneric,
+            \Praxigento\Bonus\Base\Lib\Entity\Type\Calc::class
+        );
     }
 }
