f67ebca5cf933bb2d26d3a234d414c4ed9ff0150
Alex Gusev <alex@flancer64.com>
2016-07-14 11:21:25 +0300

    MOBI-341 - Add country/region/zip attrs to the store and use its in tax calc

--- src/Lib/Entity/Type/Calc.php
+++ src/Lib/Entity/Type/Calc.php
@@ -4,12 +4,8 @@ namespace Praxigento\Bonus\Base\Lib\Entity\Type;
 /**
  * User: Alex Gusev <alex@flancer64.com>
  */
-class Calc extends \Praxigento\Core\Data\Entity\Type\Base
+class Calc
+    extends \Praxigento\Core\Data\Entity\Type\Base
 {
     const ENTITY_NAME = 'prxgt_bon_base_type_calc';
-
-    public function getEntityName()
-    {
-        return self::ENTITY_NAME;
-    }
 }
