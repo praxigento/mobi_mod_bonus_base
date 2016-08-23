ca84b97bd62294270976ddf9196c8b85e08e0b10
Alex Gusev <alex@flancer64.com>
2016-08-23 12:43:51 +0300

    MOBI-413 - Activate Mockery validation (once, etc.)

--- test/unit/Repo/Entity/Def/Rank_Test.php
+++ test/unit/Repo/Entity/Def/Rank_Test.php
@@ -10,25 +10,18 @@ use Praxigento\BonusBase\Repo\Entity\IRank;
 
 include_once(__DIR__ . '/../../../phpunit_bootstrap.php');
 
-class Rank_UnitTest extends \Praxigento\Core\Test\BaseMockeryCase
+class Rank_UnitTest
+    extends \Praxigento\Core\Test\BaseCase\Repo
 {
-    /** @var  \Mockery\MockInterface */
-    private $mConn;
-    /** @var  \Mockery\MockInterface */
-    private $mRepoGeneric;
     /** @var  Rank */
     private $obj;
 
     public function setUp()
     {
         parent::setUp();
-        /** create mocks */
-        $this->mConn = $this->_mockConn();
-        $this->mRepoGeneric = $this->_mockRepoGeneric();
         /** create object to test */
-        $mResource = $this->_mockResourceConnection($this->mConn);
         $this->obj = new Rank(
-            $mResource,
+            $this->mResource,
             $this->mRepoGeneric,
             Entity::class
         );
