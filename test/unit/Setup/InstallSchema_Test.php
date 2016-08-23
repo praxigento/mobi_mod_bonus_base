4983f499dd7aea3df6c85a8aea3833b24b168daf
Alex Gusev <alex@flancer64.com>
2016-08-23 13:43:42 +0300

    MOBI-413 - Activate Mockery validation (core)

--- test/unit/Setup/InstallSchema_Test.php
+++ test/unit/Setup/InstallSchema_Test.php
@@ -20,7 +20,7 @@ use Praxigento\Bonus\Base\Lib\Entity\Type\Calc as TypeCalc;
 include_once(__DIR__ . '/../phpunit_bootstrap.php');
 
 class InstallSchema_UnitTest
-    extends \Praxigento\Core\Test\BaseCase\InstallSchema
+    extends \Praxigento\Core\Test\BaseCase\Setup\Schema
 {
     /** @var  InstallSchema */
     private $obj;
