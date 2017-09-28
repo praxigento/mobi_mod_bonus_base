00e32a4ed1aa1d0909b2a84acd7f4e3f221bbe5a
dmitriimakhov <dmitriimakhov@gmail.com>
2017-09-28 15:26:10 +0300

    MOBI-915 # Improve Bonus Calc grid # merging with Bonus Periods grid

--- src/Ui/DataProvider/Grid/Calc/QueryBuilder.php
+++ src/Ui/DataProvider/Grid/Calc/QueryBuilder.php
@@ -3,30 +3,41 @@
 namespace Praxigento\BonusBase\Ui\DataProvider\Grid\Calc;
 
 use Praxigento\BonusBase\Repo\Entity\Data\Calculation as ECalculation;
+use Praxigento\BonusBase\Repo\Entity\Data\Period as EPeriod;
 
 class QueryBuilder
     extends \Praxigento\Core\Ui\DataProvider\Grid\Query\Builder
 {
     /**#@+ Tables aliases for external usage ('camelCase' naming) */
     const AS_BON_BASE_CALC = 'bbc';
+    const AS_BON_BASE_PERIOD = 'bbp';
     /**#@- */
-
+    const A_CALC_TYPE_ID = 'calcTypeId';
+    const A_DATE_ENDED = 'dateEnded';
     /**#@+
      * Aliases for data attributes.
      */
-    const A_ID_CALC = 'id';
-    const A_ID_PERIOD = 'periodId';
     const A_DATE_STARTED = 'dateStarted';
-    const A_DATE_ENDED = 'dateEnded';
+    const A_PERIOD = 'period';
     const A_STATE = 'state';
     /**#@- */
+
+    /**
+     * Construct expression for Period("firstName lastName").
+     */
+    public function getExpForPeriod()
+    {
+        $value = 'SUBSTR(`' . self::AS_BON_BASE_PERIOD . '`.`' . EPeriod::ATTR_DSTAMP_BEGIN . '`, 1, 6)';
+        $result = new \Praxigento\Core\Repo\Query\Expression($value);
+        return $result;
+    }
 
     protected function getMapper()
     {
         if (is_null($this->mapper)) {
             $map = [
-                self::A_ID_CALC> self::AS_BON_BASE_CALC . '.' . ECalculation::ATTR_ID,
-                self::A_ID_PERIOD => self::AS_BON_BASE_CALC . '.' . ECalculation::ATTR_PERIOD_ID,
+                self::A_PERIOD > self::AS_BON_BASE_PERIOD . '.' . $this->getExpForPeriod(),
+                self::A_CALC_TYPE_ID > self::AS_BON_BASE_PERIOD . '.' . EPeriod::ATTR_CALC_TYPE_ID,
                 self::A_DATE_STARTED => self::AS_BON_BASE_CALC . '.' . ECalculation::ATTR_DATE_STARTED,
                 self::A_DATE_ENDED => self::AS_BON_BASE_CALC . '.' . ECalculation::ATTR_DATE_ENDED,
                 self::A_STATE => self::AS_BON_BASE_CALC . '.' . ECalculation::ATTR_STATE
@@ -47,13 +58,25 @@ class QueryBuilder
         $tbl = $this->resource->getTableName(ECalculation::ENTITY_NAME);
         $as = $asCalc;
         $cols = [
-            self::A_ID_CALC => ECalculation::ATTR_ID,
-            self::A_ID_PERIOD => ECalculation::ATTR_PERIOD_ID,
             self::A_DATE_STARTED => ECalculation::ATTR_DATE_STARTED,
             self::A_DATE_ENDED => ECalculation::ATTR_DATE_ENDED,
             self::A_STATE => ECalculation::ATTR_STATE
         ];
         $result->from([$as => $tbl], $cols);
+
+        /* LEFT JOIN Period */
+        $asPeriod = self::AS_BON_BASE_PERIOD;
+        $tbl = $this->resource->getTableName(EPeriod::ENTITY_NAME);
+        $as = $asPeriod;
+        $exp = $this->getExpForPeriod();
+        $cols = [
+            self::A_PERIOD => $exp,
+            self::A_CALC_TYPE_ID => EPeriod::ATTR_CALC_TYPE_ID
+        ];
+        $cond = $as . '.' . EPeriod::ATTR_ID . '=' . $asCalc . '.' . ECalculation::ATTR_PERIOD_ID;
+        $result->joinLeft([$as => $tbl], $cond, $cols);
+
+
         return $result;
     }
 
