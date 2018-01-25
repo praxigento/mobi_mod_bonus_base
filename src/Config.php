bc545913671e3c92a27b57ccac6a0085403fe11c
Alex Gusev <alex@flancer64.com>
2018-01-25 19:10:43 +0200

    MOBI-691 Add "Processing fee" service

--- src/Config.php
+++ src/Config.php
@@ -21,7 +21,7 @@ class Config extends \Praxigento\Core\Config
     const CALC_STATE_STARTED = 'started';
 
     const CODE_TYPE_ASSET_PV = PvCfg::CODE_TYPE_ASSET_PV;
-    const CODE_TYPE_ASSET_WALLET_ACTIVE = WalletCfg::CODE_TYPE_ASSET_WALLET_ACTIVE;
+    const CODE_TYPE_ASSET_WALLET = WalletCfg::CODE_TYPE_ASSET_WALLET;
 
     const MENU_BONUS_CALC = 'bonus_calc';
     const MENU_BONUS_PERIOD = 'bonus_period';
