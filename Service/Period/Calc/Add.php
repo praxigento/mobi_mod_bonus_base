b2bbb905705d09aa6c10cad9471f10e8557ffc7e
Alex Gusev <alex@flancer64.com>
2018-08-02 18:10:44 +0300

    MOBI-1599 No PV displayed in DCP dashboard

--- Service/Period/Calc/Add.php
+++ Service/Period/Calc/Add.php
@@ -12,16 +12,16 @@ use Praxigento\BonusBase\Repo\Data\Period as EPeriod;
 class Add
     implements IAdd
 {
-    /** @var \Praxigento\Core\Api\Helper\Date */
-    protected $hlpDate;
-    /** @var \Praxigento\Core\Api\Helper\Period */
-    protected $hlpPeriod;
     /** @var \Praxigento\BonusBase\Repo\Dao\Calculation */
     protected $daoCalc;
     /** @var \Praxigento\BonusBase\Repo\Dao\Period */
     protected $daoPeriod;
     /** @var \Praxigento\BonusBase\Repo\Dao\Type\Calc */
     protected $daoTypeCalc;
+    /** @var \Praxigento\Core\Api\Helper\Date */
+    protected $hlpDate;
+    /** @var \Praxigento\Core\Api\Helper\Period */
+    protected $hlpPeriod;
 
     public function __construct(
         \Praxigento\Core\Api\Helper\Date $hlpDate,
@@ -29,8 +29,7 @@ class Add
         \Praxigento\BonusBase\Repo\Dao\Calculation $daoCalc,
         \Praxigento\BonusBase\Repo\Dao\Period $daoPeriod,
         \Praxigento\BonusBase\Repo\Dao\Type\Calc $daoTypeCalc
-    )
-    {
+    ) {
         $this->hlpDate = $hlpDate;
         $this->hlpPeriod = $hlpPeriod;
         $this->daoCalc = $daoCalc;
@@ -50,31 +49,24 @@ class Add
         /**
          * perform processing
          */
-        $dsToday = $this->hlpPeriod->getPeriodCurrent();
-        if ($dsEnd <= $dsToday) {
-            /* the end of the new period is not in the future */
-            $calcTypeId = $this->daoTypeCalc->getIdByCode($calcTypeCode);
-            /* registry new period */
-            $period = new EPeriod();
-            $period->setCalcTypeId($calcTypeId);
-            $period->setDstampBegin($dsBegin);
-            $period->setDstampEnd($dsEnd);
-            $periodId = $this->daoPeriod->create($period);
-            /* registry new calculation for the period */
-            $dateStarted = $this->hlpDate->getUtcNowForDb();
-            $calc = new ECalc();
-            $calc->setPeriodId($periodId);
-            $calc->setDateStarted($dateStarted);
-            $calc->setState(Cfg::CALC_STATE_STARTED);
-            $calcId = $this->daoCalc->create($calc);
+        $calcTypeId = $this->daoTypeCalc->getIdByCode($calcTypeCode);
+        /* registry new period */
+        $period = new EPeriod();
+        $period->setCalcTypeId($calcTypeId);
+        $period->setDstampBegin($dsBegin);
+        $period->setDstampEnd($dsEnd);
+        $periodId = $this->daoPeriod->create($period);
+        /* registry new calculation for the period */
+        $dateStarted = $this->hlpDate->getUtcNowForDb();
+        $calc = new ECalc();
+        $calc->setPeriodId($periodId);
+        $calc->setDateStarted($dateStarted);
+        $calc->setState(Cfg::CALC_STATE_STARTED);
+        $calcId = $this->daoCalc->create($calc);
 
-            /* put result data into context */
-            $ctx->set(self::CTX_OUT_PERIOD_ID, $periodId);
-            $ctx->set(self::CTX_OUT_CALC_ID, $calcId);
-            $ctx->set(self::CTX_OUT_SUCCESS, true);
-        } else {
-            /* the end of the new period is in the future - error */
-            $ctx->set(self::ERR_PERIOD_END_IS_IN_FUTURE, true);
-        }
+        /* put result data into context */
+        $ctx->set(self::CTX_OUT_PERIOD_ID, $periodId);
+        $ctx->set(self::CTX_OUT_CALC_ID, $calcId);
+        $ctx->set(self::CTX_OUT_SUCCESS, true);
     }
 }
